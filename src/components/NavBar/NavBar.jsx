244d410b876b03f18d443e702734f458d9aeb529
LukeSchaffel <lukeschaffel@gmail.com>
2022-09-28 14:41:50 -0400

    dashboard Icons

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -3,6 +3,10 @@ import { useDispatch, useSelector } from "react-redux";
 import { logout } from "../../actions/auth"
 import { useCallback } from 'react';
 import { Navbar, Container, Nav } from 'react-bootstrap';
+import { RiLoginCircleFill } from 'react-icons/ri';
+import { FaSignOutAlt, FaHome } from "react-icons/fa";
+import { MdOutlineDashboardCustomize } from "react-icons/md";
+
 
 const NavBar = () => {
   const dispatch = useDispatch()
@@ -20,24 +24,24 @@ const NavBar = () => {
       {user ?
         <Navbar bg="dark" variant="dark">
           <Container>
-            <Navbar.Brand href="/">Friday Auth Template</Navbar.Brand>
+            <Navbar.Brand href="/">Friday Auth Template <FaHome /> </Navbar.Brand>
             <Nav className="me-auto">
               {user.role === 'ADMIN' ?
-                <Nav.Link href="/dashboard">Admin Dashboard</Nav.Link>
+                <Nav.Link href="/dashboard">Admin Dashboard <MdOutlineDashboardCustomize /> </Nav.Link>
                 :
-                <Nav.Link href="/dashboard">Dashboard</Nav.Link>
+                <Nav.Link href="/dashboard" >Dashboard <MdOutlineDashboardCustomize /></Nav.Link>
               }
-              <Nav.Link href="/" onClick={logOut}>Log Out</Nav.Link>
+              <Nav.Link href="/" onClick={logOut}>Log Out <FaSignOutAlt /></Nav.Link>
             </Nav>
           </Container>
         </Navbar>
 
         :
-        <Navbar bg="info" variant="dark">
+        <Navbar bg="dark" variant="dark">
           <Container>
-            <Navbar.Brand href="/">Friday Auth Template</Navbar.Brand>
+            <Navbar.Brand href="/">Friday Auth Template <FaHome /> </Navbar.Brand>
             <Nav className="me-auto">
-              <Nav.Link href="/login">Log In</Nav.Link>
+              <Nav.Link href="/login">Log In  <RiLoginCircleFill /></Nav.Link>
               <Nav.Link href="/signup">Sign Up</Nav.Link>
             </Nav>
           </Container>
