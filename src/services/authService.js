b76c76ff3d5a4317188d6b040f880a7cfd0eec75
LukeSchaffel <lukeschaffel@gmail.com>
2022-09-28 09:49:46 -0400

    get all users api call

--- src/services/authService.js
+++ src/services/authService.js
@@ -16,16 +16,28 @@ export const signup = async (name, email, password) => {
 
 export const login = async (email, password) => {
   const res = await axios.post(`${API_URL}/login`, {
-      headers: { 'Content-Type': 'application/json' },
-      email,
-      password,
-    })
-    const data = res.data
-    if (data.accessToken) {
-      tokenService.setToken(data.accessToken)
-    }
-    return data
+    headers: { 'Content-Type': 'application/json' },
+    email,
+    password,
+  })
+  const data = res.data
+  if (data.accessToken) {
+    tokenService.setToken(data.accessToken)
+  }
+  return data
 };
+
+export const getAllUsers = async (user) => {
+  const token = tokenService.getToken()
+  const res = await axios.get(`${API_URL}`, {
+    headers: {
+      'Authorization': `Bearer ${tokenService.getToken()}`
+    }
+  })
+  const users = res.data
+  return users
+}
+
 
 export const logout = () => {
   tokenService.removeToken();
